f741998a4892d05d38aff5d0259eb11ee5789abf
EvgeniGenchev07 <zakg665@gmail.com>
2024-04-14 16:20:32 +0300

    nav bar

--- public/nav.js
+++ public/nav.js
@@ -1,30 +1,20 @@
 class Nav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <div id="myNav" class="overlay">
-                <a href="javascript:void(0)" class="closebtn" id="closeBtn">&times;</a>
-                <div class="overlay-content">
-                <a href="index.html">Начална страница</a>
-                <a href="timeline.html">Линия на времето</a>
-                <a href="artifacts.html">Артефакти</a>
-                <a href="aboutus.html">За нас</a>
-                <a href="contact.html">Контакти</a>
-            </div>
-        </div>
-        <span class="opnbtn" id="openBtn">&#9776;</span>
+        <nav>
+        <ul>
+         <li class="active">
+         <button>Animation</button>
+         </li>
+         <li>
+         <button>Branding</button>
+         </li>
+         <li>
+         <button>Illustration</button>
+         </li>
+        </ul>
+       </nav>
         `;
-
-        // Add event listeners
-        this.querySelector('#openBtn').addEventListener('click', () => this.openNav());
-        this.querySelector('#closeBtn').addEventListener('click', () => this.closeNav());
-    }
-
-    openNav() {
-        this.querySelector("#myNav").style.width = "100%";
-    }
-
-    closeNav() {
-        this.querySelector("#myNav").style.width = "0%";
     }
 }
 
