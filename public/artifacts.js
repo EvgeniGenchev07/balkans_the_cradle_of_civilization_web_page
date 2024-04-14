5e9e15c186ddc5a2613621eae75bd3597cc9f800
EvgeniGenchev07 <zakg665@gmail.com>
2024-04-14 13:01:33 +0300

    artefact pop up added

--- public/artifacts.js
+++ public/artifacts.js
@@ -1,10 +1,3 @@
-/*class Div extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = ``;
-    }
-}
-customElements.define('main-nav', Nav);
-*/
 const card_model=data=>`
 <div class="wrapper">
 
@@ -20,23 +13,27 @@ const card_model=data=>`
     <div class="back">
         <div class="inner">
             <p>${data.info}</p>
-            <button data-modal-target="#modal">Open Modal</button>
-            <div class="modal" id="modal"></div>
-            <button class="btnartf"><span>Open</span><span>Read more</span></button>
+            
+            <button  id="${data.id}" class="btnartf"><span>Open</span><span>Read more</span></button>
         </div>
     </div>
 </div> </div>
 </div>
-`
-fetch('artifacts_data.json')
+<div class="modal-container" id="${'modal-container'+data.id}">
+            <div class="modal" id="modal"></div>
+            </div>`
+fetch('assets/artifacts_data.json')
 .then(response => response.json())
 .then(data => {
     var artifacts = data.artifacts;
     for(var i = 0;i<artifacts.length;i++)
     {
-
         var artifact = artifacts[i];
-        document.getElementById("artifacts").insertAdjacentHTML('beforeend',card_model({image:artifact.image,name:artifact.name,info:artifact.short_info}));
-        console.log('green')
+        document.getElementById("artifacts").insertAdjacentHTML('beforeend',card_model({image:artifact.image,name:artifact.name,info:artifact.short_info,id:i}));
+        let modal_btn = document.getElementById(i);
+        modal_btn.addEventListener('click',()=>{      
+            //modal_btn.closest('.modal-container').classList.add('show')
+            document.getElementById('modal-container'+modal_btn.id).classList.add('show');
+        });
     }
 });
--- public/popupwindow.js
+++ public/popupwindow.js
@@ -1,56 +1,46 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const modal_btn = document.getElementById('modal_btn')
+// //const overlay = document.getElementById('overlay')
+// const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    fetch('artifacts_data.json')
-.then(response => response.json())
-.then(data => {
-    var artifacts = data.artifacts;
-    for(var i = 0;i<artifacts.length;i++)
-    {
+//   modal_btn.addEventListener('click', () => {
+//   console.log("blu");
+//   fetch('artifacts_data.json')
+// .then(response => response.json())
+// .then(data => {
+//     var artifact = data.artifacts[modal_btn.id];
+//             document.getElementById("modal").insertAdjacentHTML('beforeend',`<div class="modal-header">
+//     <div class="title">Example Modal</div>
+//     <button data-close-button class="close-button">&times;</button>
+// </div>
+// <div class="modal-body">
+//   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod alias ut illo doloremque eum ipsum obcaecati distinctio debitis reiciendis quae quia soluta totam doloribus quos nesciunt necessitatibus, consectetur quisquam accusamus ex, dolorum, dicta vel? Nostrum voluptatem totam, molestiae rem at ad autem dolor ex aperiam. Amet assumenda eos architecto, dolor placeat deserunt voluptatibus tenetur sint officiis perferendis atque! Voluptatem maxime eius eum dolorem dolor exercitationem quis iusto totam! Repudiandae nobis nesciunt sequi iure! Eligendi, eius libero. Ex, repellat sapiente!
+// </div>`);
+//       openModal(modal)
+//         }
+//       );
+//     });
+// /**/
+// overlay.addEventListener('click', () => {
+//   const modal = document.querySelector('.modal.active')
+//     closeModal(modal)
+//   })
 
-        var artifact = artifacts[i];
-        if(i===button.id)
-        {
-            document.getElementById("modal").insertAdjacentHTML('beforeend',`<div class="modal-header">
-    <div class="title">Example Modal</div>
-    <button data-close-button class="close-button">&times;</button>
-</div>
-<div class="modal-body">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quod alias ut illo doloremque eum ipsum obcaecati distinctio debitis reiciendis quae quia soluta totam doloribus quos nesciunt necessitatibus, consectetur quisquam accusamus ex, dolorum, dicta vel? Nostrum voluptatem totam, molestiae rem at ad autem dolor ex aperiam. Amet assumenda eos architecto, dolor placeat deserunt voluptatibus tenetur sint officiis perferendis atque! Voluptatem maxime eius eum dolorem dolor exercitationem quis iusto totam! Repudiandae nobis nesciunt sequi iure! Eligendi, eius libero. Ex, repellat sapiente!
-</div>`);
-        }
-    }
-});
-openModal(modal)
-})
-})
-/**/
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
+// closeModalButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
 
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
+// function openModal(modal) {
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
+// }
 
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+// function closeModal(modal) {
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
+// }
