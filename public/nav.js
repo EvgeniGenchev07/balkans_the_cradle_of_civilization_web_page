3f51b7a3c42af32aafe4ecad84cf28a13c35bd37
EvgeniGenchev07 <zakg665@gmail.com>
2024-04-14 16:52:26 +0300

    commit

--- public/nav.js
+++ public/nav.js
@@ -1,19 +1,7 @@
 class Nav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <nav>
-        <ul>
-         <li class="active">
-         <button>Animation</button>
-         </li>
-         <li>
-         <button>Branding</button>
-         </li>
-         <li>
-         <button>Illustration</button>
-         </li>
-        </ul>
-       </nav>
+        
         `;
     }
 }
