6f13a344d76f62629c5e2f42342ac89f86eebb3f
EvgeniGenchev07 <zakg665@gmail.com>
2024-04-19 00:01:44 +0300

    commit

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
 const basic_information_model = data = `
 <div class="nav-container">
-<button class="info-button" data-target="info1">${data.information_button_1}</button>
-<button class="info-button" data-target="info2">${data.information_button_2}</button>
-<button class="info-button" data-target="info3">${data.information_button_3}</button>
+<button class="info-button" data-target="info1">${data.information_title_1}</button>
+<button class="info-button" data-target="info2">${data.information_title_2}</button>
+<button class="info-button" data-target="info3">${data.information_title_3}</button>
 </div>
 
 <div class="info-container">
@@ -43,12 +43,24 @@ const basic_information_model = data = `
 </p>
 </div>
 </div>`;
+const paragraph_model=data=>`<article class="col-md-6">
+<h2 class="text-center">${data.paragraph_title}</h2>
+<p>
+  ${data.paragraph_information}
+</p>
+<p class="text-center">
+  <a href="${data.paragraph_link}" class="btn btn-primary" target="_blank">Visit the ${data.paragraph_title} Page</a>
+</p>
+</article>`;
 const fetch_and_display= object_name=>{
     fetch('./assets/index_data.json')
   .then((response) => response.json())
   .then(data=>{
     var page_data = data.object_name;
-    document.getElementById('basic_information').insertAdjacentHTML('beforeend',basic_information_model(page_data.basic_information));
+    document.getElementById('basic_information').closest('#'+object_name).insertAdjacentHTML('beforeend',basic_information_model(page_data.basic_information));
+    document.getElementById('first_paragraph').insertAdjacentHTML('beforeend',paragraph_model(page_data.first_paragraph));
+    document.getElementById('second_paragraph').closest('#'+object_name).insertAdjacentHTML('beforeend',paragraph_model(page_data.second_paragraph));
+    document.getElementById('third_paragraph').closest('#'+object_name).insertAdjacentHTML('beforeend',paragraph_model(page_data.third_paragraph));
   });
 }
 document.addEventListener("varna", () => {
