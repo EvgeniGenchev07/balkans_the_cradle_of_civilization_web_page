0f923a4fb4b9e085cc67e021728420cb4eea9251
EvgeniGenchev07 <zakg665@gmail.com>
2024-04-22 21:17:13 +0300

    commit

--- public/footer.js
+++ public/footer.js
@@ -1,13 +1,19 @@
-class Footer extends HTMLElement {
+const footer =data=> `
+<footer>            
+<p>&copy; ${data.year} ${data.name} All rights reserved.</p>         
+</footer>     
+`;
+fetch('assets/metadata.json')
+.then(response => response.json())
+.then(data => {
+  class Footer extends HTMLElement {
     connectedCallback() {
-      this.innerHTML = `
-        <footer>            
-        <p>&copy; 2024 Prehistory Website All rights reserved.</p>         
-        </footer>     
-      `;
+        this.innerHTML = footer(data.footer);
     }
   }
   customElements.define('main-footer', Footer);
+});
+
   document.getElementById("nav-toggle").addEventListener('click', ()=>{
     document.getElementById("nav_background").classList.toggle("show");
     document.getElementById("index_navigation").classList.toggle("hide");
